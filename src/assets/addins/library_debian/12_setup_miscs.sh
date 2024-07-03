7679f855fd77601fd988b0d0471e46d59685b67f
Wilson <vu.minh.quang@outlook.com>
2024-07-03 07:59:24 +0700

    Add whole double_commander setting files

--- src/assets/addins/library_debian/12_setup_miscs.sh
+++ src/assets/addins/library_debian/12_setup_miscs.sh
@@ -8,6 +8,14 @@ echo_message() {
 # Install zlib
 echo_message "Installing zlib..."
 sudo apt-get update
-sudo apt-get install -y zlib1g-dev libsecret-1-0 gnome-keyring || { echo_message "Failed to install zlib."; exit 1; }
+sudo apt-get install -y console-setup zlib1g-dev || { echo_message "Failed to install zlib."; exit 1; }
 
 echo_message "zlib installation complete."
+
+
+# Install gnome-keyring
+echo_message "Installing gnome-keyring..."
+sudo apt-get update
+sudo apt-get install -y libsecret-1-0 gnome-keyring || { echo_message "Failed to install gnome-keyring."; exit 1; }
+
+echo_message "gnome-keyring installation complete."
